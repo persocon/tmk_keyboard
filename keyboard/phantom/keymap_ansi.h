346ee01fde9f422d069dc9facd5b359097f8b7b6
Pedro Barbosa Costa Neves <pedro@persocon.org>
2015-10-28 18:07:59 -0200

    adding EJCT to lock screen

--- keyboard/phantom/keymap_ansi.h
+++ keyboard/phantom/keymap_ansi.h
@@ -2,11 +2,11 @@
 static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: qwerty */
     KEYMAP_ANSI(\
-        ESC,      F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,       PSCR,SLCK,BRK,  \
+        ESC,      F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,       PSCR,SLCK,EJCT, \
         GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,      INS, HOME,PGUP, \
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,      DEL, END, PGDN, \
         FN0, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,       FN1, FN2, FN3,  \
-        LSFT,     Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,      FN4, UP, FN5,   \
+        LSFT,     Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,      FN4, UP, F20,   \
         LCTL,LALT,LGUI,               SPC,                RGUI,RALT,RCTL, APP,      LEFT,DOWN,RGHT),
     /* 1: media keys */
     KEYMAP_ANSI(\
@@ -27,7 +27,7 @@ static const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),
 
     [2] = ACTION_MACRO(LIGHTROOM_EXPORT),              // Macro: Lightroom Export
-    [1] = ACTION_MACRO(LIGHTROOM_TOGGLE_LIB_DEV)       // Macro: Lightroom Develop Library Toggle
+    [1] = ACTION_MACRO(LIGHTROOM_TOGGLE_LIB_DEV),       // Macro: Lightroom Develop Library Toggle
 
 };
 
